ca805015a9b73b0400a2d080c5ea589d42768fb8
Renan Aragão <refaragao@gmail.com>
2025-04-18 19:15:31 -0300

    finzalizado a implmentação do heapsort

--- src/__main__.py
+++ src/__main__.py
@@ -1,19 +1,44 @@
 #!/usr/bin/env python3
 
 import sys
+
 from simple_term_menu import TerminalMenu
-from sort.quicksort import quicksort
+
 from sort.bubblesort import bubblesort
+from sort.quicksort import quicksort
+from sort.heapsort import BinaryHeap
 
 actions = {0: bubblesort, 1: quicksort}
 
 
 def main():
-    options = ["bubblesort", "quicksort"]
+    options = ["bubblesort", "quicksort", "heapsort"]
     terminal_menu = TerminalMenu(options)
 
     index = terminal_menu.show()
-    print(actions[index]([7, 5, 10, 6, 8]))
+
+    if index in actions:
+        print(actions[index]([8, 5, 10, 6, 8]))
+
+    heap = BinaryHeap()
+
+    values = [4, 2, 8, 7, 1, 5, 3, 6, 10]
+
+    for value in values:
+        heap.insert(value)
+
+    print(heap._data)
+
+    print(heap.dequeue())
+    print(heap.dequeue())
+    print(heap.dequeue())
+    print(heap.dequeue())
+    print(heap.dequeue())
+    print(heap.dequeue())
+    print(heap.dequeue())
+    print(heap.dequeue()) 
+
+    print(BinaryHeap.heapSort(values))
 
 
 if __name__ == "__main__":
--- src/sort/heapsort.py
+++ src/sort/heapsort.py
@@ -3,6 +3,10 @@ class BinaryHeap:
 
     def insert(self, value):
         self._data.append(value)
+
+        if len(self._data) == 1:
+            return None
+
         i = len(self._data) - 1
         p = BinaryHeap.parent(i)
 
@@ -14,27 +18,51 @@ class BinaryHeap:
             i = p
             p = BinaryHeap.parent(i)
 
+            if p == -1:
+                break
+
+    def dequeue(self):
+        if len(self._data) == 0:
+            return None
+
+        result = self._data[0]
+
+        self._data.pop(len(self._data) - 1)
+        self._data[0] = self._data[len(self._data) - 1]
+
+        BinaryHeap._heapfy(self._data, len(self._data), 0)
+
+        return result
+
+    @staticmethod
+    def heapSort(data):
+        BinaryHeap.heapfy(data, len(data))
+
+        for i in range(len(data) - 1, -1, -1):
+            BinaryHeap.swap(data, 0, i)
+
+            BinaryHeap._heapfy(data, (i - 1), 0)
+
+        return data
+
     @staticmethod
     def parent(nodeIndex):
-        return nodeIndex == 0 if -1 else (nodeIndex - 1) // 2
+        if nodeIndex == 0:
+            return -1
+
+        return (nodeIndex - 1) // 2
 
     @staticmethod
     def isBigger(valorA, valorB):
         return valorA > valorB
 
     @staticmethod
-    def returnResult(result, data):
-        return (result < len(data)) if result else -1
-
-    @staticmethod
     def heapfy(data, size):
         if size <= 1:
             return None
 
-        lastIndex = size - 1
-
-        # O lastIndex / 2 - 1 é para pegar o último nó que tem filhos.
-        for i in range(lastIndex / 2 - 1, -1, -1):
+        # O size / 2 - 1 é para pegar o último nó que tem filhos.
+        for i in range(size // 2 - 1, -1, -1):
             BinaryHeap._heapfy(data, size, i)
 
     @staticmethod
@@ -43,11 +71,14 @@ class BinaryHeap:
         left = BinaryHeap.left(data, index)
         right = BinaryHeap.right(data, index)
 
-        if left < size & BinaryHeap.isBigger(data[left], data[biggest]):
+        if left < size and BinaryHeap.isBigger(data[left], data[biggest]):
             biggest = left
 
-        if right < size & BinaryHeap.isBigger(data[right], data[biggest]):
+        if right < size and BinaryHeap.isBigger(data[right], data[biggest]):
             biggest = right
+
+        if biggest == -1:
+            return None
 
         if biggest != index:
             BinaryHeap.swap(data, index, biggest)
@@ -65,7 +96,13 @@ class BinaryHeap:
 
     @staticmethod
     def swap(data, indexA, indexB):
-        (data[indexA], data[indexB]) = (
-            data[indexB],
-            data[indexA],
-        )
+        data[indexA], data[indexB] = data[indexB], data[indexA]
+
+        return data
+
+    @staticmethod
+    def returnResult(result, data):
+        if result < len(data):
+            return result
+
+        return -1
